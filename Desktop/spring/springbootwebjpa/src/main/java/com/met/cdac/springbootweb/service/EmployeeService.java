fa485d879c1f46d0c101ee91630ce01720ab5367
jrushi2010 <jrushi2010@gmail.com>
2023-06-17 16:42:26 +0530

    added jpaRepository interface and done changes in to the employeeService class

--- Desktop/spring/springbootwebjpa/src/main/java/com/met/cdac/springbootweb/service/EmployeeService.java
+++ Desktop/spring/springbootwebjpa/src/main/java/com/met/cdac/springbootweb/service/EmployeeService.java
@@ -5,17 +5,25 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.met.cdac.springbootweb.dao.EmployeeDao;
+import com.met.cdac.springbootweb.dao.EmployeeDaoRepository;
 import com.met.cdac.springbootweb.model.Employee;
 
 
 @Service
 public class EmployeeService {
 	
-	@Autowired
-	EmployeeDao empdao;
+//	@Autowired
+//	EmployeeDao empdao;
 	
+	@Autowired
+	private EmployeeDaoRepository employeeDaoRepository;
+	
+	
+	
+	@Transactional
 	public void saveEmployee(Employee employee) {
 		
 		//System.out.println(empdao.getClass());
@@ -30,19 +38,27 @@ public class EmployeeService {
 		
 		System.out.println("EmployeeService :: saveEmployee");
 		
-		empdao.saveEmployee(employee);
+		//empdao.saveEmployee(employee);
+		
+		employeeDaoRepository.save(employee);
 	
 	}
 	
+	@Transactional(readOnly = true)
 	public Collection<Employee> getAllEmployees(){
 		
 		//return empdao.getAllEmployees();
-		return empdao.getAllEmployeesUsingJpa();
+		//return empdao.getAllEmployeesUsingJpa();
+		return employeeDaoRepository.findAll();
 	}
 	
+	@Transactional
 	public Employee getEmployee(int id) {
 		//return empdao.getEmployee(id);
-		return empdao.getEmployeeUsingJpa(id);
+		//return empdao.getEmployeeUsingJpa(id);
+		return employeeDaoRepository.findById(id).get();
 	}
+	
+	
 
 }
